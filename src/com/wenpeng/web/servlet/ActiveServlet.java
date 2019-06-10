44e10d05ac41d10ed6d93d345dbf85430bd57cce
wenpen123 <582577209@qq.com>
2019-06-10 21:58:20 +0800

    1.0

--- src/com/wenpeng/web/servlet/ActiveServlet.java
+++ src/com/wenpeng/web/servlet/ActiveServlet.java
@@ -1,8 +1,8 @@
 package com.wenpeng.web.servlet;
 
+import com.wenpeng.exception.UserException;
 import com.wenpeng.service.UserService;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,18 +11,27 @@ import java.io.IOException;
 
 @WebServlet("/active")
 public class ActiveServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
     {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
         //获取参数
         String activeCode = request.getParameter("activeCode");
         //调用业务方法
-        UserService us= new UserService();
-        us.activeUser(activeCode);
+        UserService us = new UserService();
+        try
+        {
+            us.activeUser(activeCode);
+            response.getWriter().write("激活成功");
+        }
+        catch (UserException | IOException e)
+        {
+            e.printStackTrace();
+            response.getWriter().write("激活失败");
+        }
 
     }
 }
